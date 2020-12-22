76f098ed27169609286d44fbd5ea91e606caf0d1
CUI Zheng <blbhy2825@live.com>
2020-12-22 14:44:03 +0100

    add make choice function

--- app/src/main/java/com/cuizheng/justpickone/ChoiceAdapter.java
+++ app/src/main/java/com/cuizheng/justpickone/ChoiceAdapter.java
@@ -1,11 +1,13 @@
 package com.cuizheng.justpickone;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -13,9 +15,11 @@ import java.util.List;
 public class ChoiceAdapter extends RecyclerView.Adapter<ChoiceAdapter.ChoiceViewHolder> {
 
     private List<String> choices;
+    private List<Integer> choicesColors;
 
-    public ChoiceAdapter(List<String> choices) {
+    public ChoiceAdapter(List<String> choices, List<Integer> choicesColors) {
         this.choices = choices;
+        this.choicesColors = choicesColors;
     }
 
     @NonNull
@@ -29,6 +33,7 @@ public class ChoiceAdapter extends RecyclerView.Adapter<ChoiceAdapter.ChoiceView
     @Override
     public void onBindViewHolder(@NonNull ChoiceAdapter.ChoiceViewHolder holder, int position) {
         holder.choiceNameText.setText(choices.get(position));
+        holder.choiceCard.setBackgroundColor(choicesColors.get(position));
     }
 
     @Override
@@ -39,10 +44,14 @@ public class ChoiceAdapter extends RecyclerView.Adapter<ChoiceAdapter.ChoiceView
     public class ChoiceViewHolder extends RecyclerView.ViewHolder {
 
         TextView choiceNameText;
+        CardView choiceCard;
+        View itemView;
 
         public ChoiceViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.itemView = itemView;
             choiceNameText = itemView.findViewById(R.id.choice_name_text);
+            choiceCard = itemView.findViewById(R.id.choice_card);
         }
     }
 }
--- app/src/main/java/com/cuizheng/justpickone/MainActivity.java
+++ app/src/main/java/com/cuizheng/justpickone/MainActivity.java
@@ -2,21 +2,23 @@ package com.cuizheng.justpickone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView choicesRecyclerView;
     private List<String> choices;
+    private List<Integer> choicesColors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +26,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initialize choices list
         choices = new ArrayList<>();
+        choicesColors = new ArrayList<>();
 
         // Create recycler view
         choicesRecyclerView = findViewById(R.id.choicesRecyclerView);
-        ChoiceAdapter choiceAdapter = new ChoiceAdapter(choices);
+        ChoiceAdapter choiceAdapter = new ChoiceAdapter(choices, choicesColors);
         choicesRecyclerView.setAdapter(choiceAdapter);
         choicesRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
 
-        // Create button onClick listener
+        // Create add new choice button onClick listener
         EditText newChoiceText = findViewById(R.id.new_choice_text);
         Button addNewChoiceButton = findViewById(R.id.add_new_choice_button);
-        addNewChoiceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        addNewChoiceButton.setOnClickListener(v -> {
+            if(!newChoiceText.getText().toString().isEmpty()){
                 choices.add(newChoiceText.getText().toString());
-                newChoiceText.clearComposingText();
+                choicesColors.add(Color.WHITE);
+                newChoiceText.getText().clear();
                 choiceAdapter.notifyItemChanged(choices.size() - 1);
+            }
+        });
+
+        // Create clear all button onClick listener
+        Button clearAllButton = findViewById(R.id.clear_all_button);
+        clearAllButton.setOnClickListener(v -> {
+            if(choices.size() > 0){
+                choices.clear();
+                choiceAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Create choose button onClick listener
+        Button chooseButton = findViewById(R.id.choose_button);
+        chooseButton.setOnClickListener(v -> {
+            if(choices.size() > 0){
+                Collections.fill(choicesColors, Color.WHITE);
+                int chosenItemIndex = new Random().nextInt(choices.size());
+                choicesColors.set(chosenItemIndex, Color.RED);
+                choiceAdapter.notifyDataSetChanged();
             }
         });
     }
